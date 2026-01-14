61f6d3fed596102f06193d32fc41b081e554eb58
Janendra de silva <your-github-email@example.com>
2026-01-14 07:28:14 +0530

    Initial commit

--- src/main/java/org/example/vehiclerentalmanagementsystem/mapper/UserMapper.java
+++ src/main/java/org/example/vehiclerentalmanagementsystem/mapper/UserMapper.java
@@ -1,45 +1,4 @@
 package org.example.vehiclerentalmanagementsystem.mapper;
 
-import org.example.vehiclerentalmanagementsystem.dto.Request.UserRequest;
-import org.example.vehiclerentalmanagementsystem.dto.UserDTO;
-import org.example.vehiclerentalmanagementsystem.entity.Role;
-import org.example.vehiclerentalmanagementsystem.entity.User;
-import org.springframework.stereotype.Component;
-
-@Component
 public class UserMapper {
-    private UserMapper(){
-
-    }
-    public static User toEntity(UserRequest request){
-        if(request == null){
-            return null;
-        }
-        User user = new User();
-        user.setUsername(request.getUserName());
-        user.setPassword(request.getPassword());
-        user.setEmail(request.getEmail());
-        
-        // Set role from request, default to USER if not provided or invalid
-        try {
-            user.setRole(Role.valueOf(request.getRole().toUpperCase()));
-        } catch (Exception e) {
-            user.setRole(Role.USER);
-        }
-        
-        return user;
-    }
-
-    public static UserDTO toDTO(User user){
-        if (user == null){
-            return null;
-        }
-        UserDTO dto = new UserDTO();
-        dto.setId(user.getId());
-        dto.setUserName(user.getUsername());
-        dto.setEmail(user.getEmail());
-        dto.setRole(user.getRole().toString());
-        return dto;
-
-    }
 }
